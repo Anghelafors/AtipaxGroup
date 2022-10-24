75870194714522c3275029465038613d35002e83
Anghela Sanchez Arias <sccp.asanchez03@gmail.com>
2022-10-23 21:19:06 -0500

    se modifica el layout

---
+++ ProjectAtipax/ProjectAtipax/Controllers/ViajesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAtipax.Controllers
+{
+    public class ViajesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
