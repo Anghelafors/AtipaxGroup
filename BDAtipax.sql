9b22191335b5b2e18a380eac21819bd65d1ce7c5
Anghela Sanchez Arias <sccp.asanchez03@gmail.com>
2022-10-25 13:41:28 -0500

    subida de bd

---
+++ BDAtipax.sql
@@ -0,0 +1,62 @@
+drop database if exists BDAtipax
+create database BDAtipax
+use BDAtipax
+--use master
+
+-- tables
+create table tb_tour(
+idTour char(5) primary key not null,
+precio decimal(7,2) not null,
+descripcion nvarchar(100) not null
+)
+go
+create table tb_hotel(
+idHotel char(5) primary key not null,
+nomHotel nvarchar(20) not null,
+categoria nvarchar(15) not null,
+precioHotel decimal(6,2) not null,
+descripcion nvarchar(50) not null,
+idTour char(5) not null,
+foreign key(idTour) references tb_tour(idTour)
+)
+go
+create table tb_destino(
+idDestino char(5) primary key not null,
+pais nvarchar(40) not null,
+ciudad nvarchar(40) not null,
+idHotel char(5) not null,
+foreign key(idHotel) references tb_hotel(idHotel)
+)
+go
+
+create table tb_cliente(
+idCliente char(6) primary key not null,
+nombre nvarchar(40)not null,
+apePaterno nvarchar(20) not null,
+apeMaterno nvarchar(20) not null,
+dni char(8) not null,
+telefono char(9) not null,
+correo nvarchar(40) not null 
+)
+go
+
+create table tb_usuarios(
+idUsu int primary key  identity(1,1) not null,
+usuario nvarchar(13) not null,
+pass nvarchar(15) not null
+)
+go
+
+/*
+tb_compra
+idCompra
+cantidadPerson
+total
+fechaInicio
+fechaFin
+idHotel
+idTour
+idDestino
+idCliente
+
+*/
