cffd9599e14bde36ed8c07e9122f560cc1c3487e
Darío Yaranga Verano <113160438+VeranoD@users.noreply.github.com>
2022-10-26 22:56:51 -0500

    Update BDAtipax.sql

--- BDAtipax.sql
+++ BDAtipax.sql
@@ -1,6 +1,7 @@
 drop database if exists BDAtipax
 create database BDAtipax
 use BDAtipax
+
 --use master
 
 -- tables
@@ -28,6 +29,13 @@ idHotel char(5) not null,
 foreign key(idHotel) references tb_hotel(idHotel)
 )
 go
+insert into tb_destino values('D0001','Peru','Lima','H0001')
+
+							 
+
+go
+select * from tb_destino
+
 
 create table tb_cliente(
 idCliente char(6) primary key not null,
@@ -39,6 +47,10 @@ telefono char(9) not null,
 correo nvarchar(40) not null 
 )
 go
+insert into tb_cliente values('C00001','Dario','Yaranga','Verano','12345678','987654327','Veranod@atipax.com')
+insert into tb_cliente values('C00002','Anghela','Sanchez','Castillo','47345678','927684321','Sanchez@atipax.com')
+insert into tb_cliente values('C00003','Marco','Castañeda','Solis','42345678','917684326','Castañeda@atipax.com')
+
 
 create table tb_usuarios(
 idUsu int primary key  identity(1,1) not null,
@@ -64,3 +76,33 @@ foreign key(idDestino) references tb_destino(idDestino),
 foreign key(idCliente) references tb_cliente(idCliente)
 )
 go
+
+create procedure usp_usuarios_listar
+	as
+	select * from tb_usuarios
+	go
+
+create procedure usp_tour_listar
+	as
+	select * from tb_tour
+	go
+
+create procedure usp_hotel_listar
+	as
+	select * from tb_hotel
+	go
+
+create procedure usp_destino_listar
+	as
+	select * from tb_destino
+	go
+
+create procedure usp_cliente_listar
+	as
+	select * from tb_cliente
+	go
+
+create procedure usp_compra_listar
+	as
+	select * from tb_compra
+	go
