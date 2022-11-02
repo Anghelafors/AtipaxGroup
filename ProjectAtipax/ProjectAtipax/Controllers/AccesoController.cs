7ea5e2981648f37cdf4d1bf4087153f754c88e53
Anghela Sanchez Arias <sccp.asanchez03@gmail.com>
2022-11-02 15:49:53 -0500

    paginas diseñadas: inicio,contactenos y escoje destino

---
+++ ProjectAtipax/ProjectAtipax/Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAtipax.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+    }
+}
--- ProjectAtipax/ProjectAtipax/Controllers/ViajesController.cs
+++ ProjectAtipax/ProjectAtipax/Controllers/ViajesController.cs
@@ -4,9 +4,22 @@ namespace ProjectAtipax.Controllers
 {
     public class ViajesController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Inicio()
         {
             return View();
         }
+        public IActionResult Contactenos()
+        {
+            return View();
+        }
+        public IActionResult EscojeDestino()
+        {
+            return View();
+        }
+        /*public IActionResult Salir()
+        {
+            return View();
+        }*/
+
     }
 }
