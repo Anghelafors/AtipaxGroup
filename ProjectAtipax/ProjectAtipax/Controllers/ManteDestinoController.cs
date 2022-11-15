47eaa780815c5dd34764624a87f3fa3f2cbe9949
Anghela Sanchez Arias <sccp.asanchez03@gmail.com>
2022-11-14 22:37:13 -0500

    actualizacion de manteDetino

--- ProjectAtipax/ProjectAtipax/Controllers/ManteDestinoController.cs
+++ ProjectAtipax/ProjectAtipax/Controllers/ManteDestinoController.cs
@@ -9,35 +9,76 @@ using System.Data;
 
 namespace ProjectAtipax.Controllers
 {
-    // [Authorize]
+    [Authorize]
     public class ManteDestinoController : Controller
     {
         IHotel _hotel;
         IDestino _destino;
+        ICategoria _categoria;
         public ManteDestinoController()
         {
 
             _hotel = new hotelDAO();
             _destino = new destinoDAO();
+            _categoria = new categoriaDAO();
         }
         //  [Authorize(Roles = "Administrador")]
         public IActionResult Create()
         {
 
             //enviar lista de hoteles
+            ViewBag.categoriaLis = new SelectList(_categoria.listado(), "IdCategoria", "NombreCategoria");
+
             ViewBag.hotelLis = new SelectList(_hotel.listado(), "idHotel", "nomHotel");
             ViewBag.destinos = _destino.listado();
             return View(new Destino());
 
         }
         [HttpPost]
-        public IActionResult Create(Destino des)
+        public IActionResult Create(Destino d)
         {
 
-            ViewBag.mensaje = _destino.agregar(des);
-            ViewBag.hotelLis = new SelectList(_hotel.listado(), "idHotel", "nomHotel", des.idHotel);
+            ViewBag.mensaje = _destino.agregar(d);
+            ViewBag.categoriaLis = new SelectList(_categoria.listado(), "IdCategoria", "NombreCategoria", d.IdCategoria);
+
+            ViewBag.hotelLis = new SelectList(_hotel.listado(), "idHotel", "nomHotel", d.idHotel);
             ViewBag.destinos = _destino.listado();
-            return View(des);
+            return View(d);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        public IActionResult Edit(int id)
+        {
+            Destino d = _destino.buscar(id);
+
+            //  if (t == null) return RedirectToAction("Index");
+            ViewBag.categoriaLis = new SelectList(_categoria.listado(), "IdCategoria", "NombreCategoria", d.IdCategoria);
+
+            ViewBag.hotelLis = new SelectList(_hotel.listado(), "idHotel", "nomHotel", d.idHotel);
+
+
+            return View(d);
+        }
+        [HttpPost]
+        public IActionResult Edit(Destino d)
+        {
+
+            ViewBag.mensaje = _destino.actualizar(d);
+            ViewBag.categoriaLis = new SelectList(_categoria.listado(), "IdCategoria", "NombreCategoria", d.IdCategoria);
+
+            ViewBag.hotelLis = new SelectList(_hotel.listado(), "idHotel", "nomHotel", d.idHotel);
+            return View(d);
+        }
+        [Authorize(Roles = "Administrador")]
+        public IActionResult Delete(int id)
+        {
+            _destino.eliminar(id);
+
+
+            return RedirectToAction("Create", "ManteDestino");
+
         }
     }
+
+
 }
